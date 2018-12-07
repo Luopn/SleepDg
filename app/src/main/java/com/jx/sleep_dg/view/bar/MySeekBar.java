cf74625f7079fe368c5ac59fc18b1bab2f5b48db
luopl <1004633425@qq.com>
2018-12-07 08:45:46 +0800

    软硬调节页面优化

--- app/src/main/java/com/jx/sleep_dg/view/bar/MySeekBar.java
+++ app/src/main/java/com/jx/sleep_dg/view/bar/MySeekBar.java
@@ -30,7 +30,7 @@ public class MySeekBar extends View {
     //滑动的圆弧颜色
     int slideRingCorlor;
     //同心圆的外圆半径
-    int radius;
+    float radius;
     //中间字的颜色
 //    int wordCorlor;
     //中间字的大小
@@ -48,6 +48,7 @@ public class MySeekBar extends View {
     double addOrReduce = 1;
     //开始滑动的起始位置度数，顶部270 右侧0 底部90 左侧180,因为这是半圆直接写死从左侧180开始滑动
     int beginLocation;
+    int sweepLocation;
     //当前可滑动区域的范围
     int slideAbleLocation;
 
@@ -64,17 +65,12 @@ public class MySeekBar extends View {
 //    int scaleLineLength;
     //线条的宽度
     int scaleLineWidth;
-    //需要特殊处理的刻度线长度，例如正方位上的刻度或者当前刻度
-    int specialScaleLineLength;
     //刻度结束的角度
     float sweepAngle = 180;//因为是半圆，这里就写死了
     //未选择的刻度线颜色
     int scaleLineNormalCorlor;
     //滑动后的刻度线颜色
     int specialScaleCorlor;
-    //刻度线距离里面的环的距离
-    int scaleToRingSpace;
-
     //画底部背景环的画笔
     Paint ringBgPaint;
     //画上面圆弧的画笔
@@ -162,19 +158,15 @@ public class MySeekBar extends View {
         ringWidth = array.getInt(R.styleable.MySeekBar_ringWidth, CommonUtil.dip2px(context, 10));
         slideRingCorlor = array.getInt(R.styleable.MySeekBar_slideRingCorlor, 0xFF6a6aff);
         ringBgCorlor = array.getInt(R.styleable.MySeekBar_ringBgCorlor, 0xFFbebebe);
-        radius = array.getInt(R.styleable.MySeekBar_radius, CommonUtil.dip2px(context, 100));
-//        wordCorlor = array.getInt(R.styleable.MyHalfCircularSildeView_wordCorlor, Color.BLUE);
-//        wordSize = array.getInt(R.styleable.MyHalfCircularSildeView_wordSize, 18);
+        radius = array.getDimension(R.styleable.MySeekBar_radius, CommonUtil.dip2px(context, 100));
+        // wordCorlor = array.getInt(R.styleable.MyHalfCircularSildeView_wordCorlor, Color.BLUE);
+        // wordSize = array.getInt(R.styleable.MyHalfCircularSildeView_wordSize, 18);
         maxProgress = array.getInt(R.styleable.MySeekBar_maxProgress, 100);
         minProgress = array.getInt(R.styleable.MySeekBar_minProgress, 0);
         progress = array.getInt(R.styleable.MySeekBar_progress, 0);
         //因为这是个半弧，所以我们直接写死了，从左侧开始
-        beginLocation = 90;
-        //下面是刻度线的属性
-//        scaleLineCount = array.getInt(R.styleable.MyHalfCircularSildeView_scaleLineCount, 100);
-//        scaleLineLength = array.getInt(R.styleable.MyHalfCircularSildeView_scaleLineLength, CommonUtil.dip2px(context, 10));
-        specialScaleLineLength = array.getInt(R.styleable.MySeekBar_specialScaleLineLength, CommonUtil.dip2px(context, 15));
-        scaleToRingSpace = array.getInt(R.styleable.MySeekBar_scaleToRingSpace, CommonUtil.dip2px(context, 10));
+        beginLocation = 110;
+        sweepLocation = 140;
         scaleLineNormalCorlor = array.getInt(R.styleable.MySeekBar_scaleLineNormalCorlor, 0xFFbebebe);
         specialScaleCorlor = array.getInt(R.styleable.MySeekBar_specialScaleCorlor, 0xFF6a6aff);
         scaleLineWidth = array.getInt(R.styleable.MySeekBar_scaleLineWidth, CommonUtil.dip2px(context, 2));
@@ -204,25 +196,18 @@ public class MySeekBar extends View {
         slideRingPaint.setStrokeCap(Paint.Cap.ROUND);//圆形笔头
 
         //写中间文字的画笔初始化
-//        wordPaint = new Paint();
-//        wordPaint.setColor(wordCorlor);
-//        wordPaint.setTextSize(CommonUtil.sp2px(context, wordSize));
-//        rect = new Rect();
-//        String str = progress+" C";
-//        wordPaint.getTextBounds(str, 0, str.length(),rect);
+        // wordPaint = new Paint();
+        // wordPaint.setColor(wordCorlor);
+        // wordPaint.setTextSize(CommonUtil.sp2px(context, wordSize));
+        // rect = new Rect();
+        // String str = progress+" C";
+        // wordPaint.getTextBounds(str, 0, str.length(),rect);
 
         //设置圆环上圆圈的画笔初始化
         mBitmapPaint = new Paint();
         mBitmapPaint.setDither(true);//设置防抖动
         mBitmapPaint.setFilterBitmap(true);//对Bitmap进行滤波处理
         mBitmapPaint.setAntiAlias(true);//设置抗锯齿
-
-        //这是画外面刻度线的画笔
-//        scalePaint = new Paint();
-//        scalePaint.setColor(scaleLineNormalCorlor);
-//        scalePaint.setAntiAlias(true);
-//        scalePaint.setStyle(Paint.Style.STROKE);
-//        scalePaint.setStrokeWidth(scaleLineWidth);
 
         //这是画滑动后的刻度线的画笔
         specialScalePaint = new Paint();
@@ -242,7 +227,7 @@ public class MySeekBar extends View {
         switch (widthModel) {
             case MeasureSpec.UNSPECIFIED:
             case MeasureSpec.AT_MOST:
-                widthSize = 2 * radius + getPaddingLeft() + getPaddingRight() + 2 * (specialScaleLineLength + scaleToRingSpace);
+                widthSize = (int) (2 * radius + getPaddingLeft() + getPaddingRight());
                 break;
             case MeasureSpec.EXACTLY:
                 break;
@@ -251,7 +236,7 @@ public class MySeekBar extends View {
             case MeasureSpec.UNSPECIFIED:
             case MeasureSpec.AT_MOST:
                 //当控件自适应时候，尺寸=半径+左右边距
-                heightSize = radius + getPaddingTop() + getPaddingBottom() + bitmapHight / 4 + specialScaleLineLength + scaleToRingSpace;
+                heightSize = (int) (radius + getPaddingTop() + getPaddingBottom() + bitmapHight / 4);
                 break;
             //当宽度全屏或者固定尺寸时候
             case MeasureSpec.EXACTLY:
@@ -265,34 +250,34 @@ public class MySeekBar extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         //画背景圆环
-        canvas.drawArc(new RectF(ringWidth / 2 + getPaddingLeft() + specialScaleLineLength + scaleToRingSpace
-                        , ringWidth / 2 + getPaddingTop() + specialScaleLineLength + scaleToRingSpace
-                        , 2 * radius - ringWidth / 2 + getPaddingLeft() + specialScaleLineLength + scaleToRingSpace
-                        , 2 * radius - ringWidth / 2 + getPaddingTop() + specialScaleLineLength + scaleToRingSpace)
-                , beginLocation, 180, false, ringBgPaint);
+        canvas.drawArc(new RectF(ringWidth / 2 + getPaddingLeft()
+                        , ringWidth / 2 + getPaddingTop()
+                        , 2 * radius - ringWidth / 2 + getPaddingLeft()
+                        , 2 * radius - ringWidth / 2 + getPaddingTop())
+                , beginLocation, 140, false, ringBgPaint);
         //画滑动圆弧
-        canvas.drawArc(new RectF(ringWidth / 2 + getPaddingLeft() + specialScaleLineLength + scaleToRingSpace
-                        , ringWidth / 2 + getPaddingTop() + specialScaleLineLength + scaleToRingSpace
-                        , 2 * radius - ringWidth / 2 + getPaddingLeft() + specialScaleLineLength + scaleToRingSpace
-                        , 2 * radius - ringWidth / 2 + getPaddingTop() + specialScaleLineLength + scaleToRingSpace)
+        canvas.drawArc(new RectF(ringWidth / 2 + getPaddingLeft()
+                        , ringWidth / 2 + getPaddingTop()
+                        , 2 * radius - ringWidth / 2 + getPaddingLeft()
+                        , 2 * radius - ringWidth / 2 + getPaddingTop())
                 , beginLocation, progress * 180 / 100, false, slideRingPaint);
         //画上滑动图标
-        PointF progressPoint = CommonUtil.calcArcEndPointXY(radius + getPaddingLeft() + specialScaleLineLength + scaleToRingSpace
-                , radius + getPaddingTop() + specialScaleLineLength + scaleToRingSpace
+        PointF progressPoint = CommonUtil.calcArcEndPointXY(radius + getPaddingLeft()
+                , radius + getPaddingTop()
                 , radius - ringWidth / 2
-                , progress * 180 / 100, 180);
+                , progress * (sweepLocation - beginLocation) / 100, beginLocation);
         int left = (int) progressPoint.x - mDragBitmap.getWidth() / 2;
         int top = (int) progressPoint.y - mDragBitmap.getHeight() / 2;
         canvas.drawBitmap(mDragBitmap, left, top, mBitmapPaint);
 
-//        onSeekBarItemClick(realShowProgress);
+        // onSeekBarItemClick(realShowProgress);
         onSeekBar.onSeekBarClick((int) realShowProgress);
         //展示进度
-//        String str = (int)Math.floor(realShowProgress)+" C";//整数
-//        String str = realShowProgress + " C";//小数
-//        wordPaint.getTextBounds(str, 0, str.length(),rect);
-//        canvas.drawText(str, radius+getPaddingLeft() + specialScaleLineLength + scaleToRingSpace-rect.width()/2
-//                , radius + getPaddingTop() + specialScaleLineLength + scaleToRingSpace, wordPaint);
+        // String str = (int)Math.floor(realShowProgress)+" C";//整数
+        // String str = realShowProgress + " C";//小数
+        // wordPaint.getTextBounds(str, 0, str.length(),rect);
+        // canvas.drawText(str, radius+getPaddingLeft() + specialScaleLineLength -rect.width()/2
+        //         , radius + getPaddingTop() + specialScaleLineLength , wordPaint);
         //画背景刻度
         paintScale(canvas);
     }
@@ -303,31 +288,10 @@ public class MySeekBar extends View {
     private void paintScale(Canvas canvas) {
         canvas.save();
         //将坐标系移到圆中心
-        canvas.translate(radius + getPaddingLeft() + specialScaleLineLength + scaleToRingSpace
-                , radius + getPaddingTop() + specialScaleLineLength + scaleToRingSpace);
+        canvas.translate(radius + getPaddingLeft()
+                , radius + getPaddingTop());
         //旋转坐标系
         canvas.rotate(90);
-//        for(int i=0; i< scaleLineCount +1; i++){
-//            //刻度线的实际展示长度
-//            int scaleLine = scaleLineLength;
-//            //正方位的线比较长
-//            if(i == 0 || i == scaleLineCount/2 || i== scaleLineCount){
-//                scaleLine = specialScaleLineLength;
-//            }
-//            //画已经滑动过的刻度线,因为实际刻度数量都是按着100个来转换的
-//            if(i * (100/scaleLineCount) <= progress){
-//                canvas.drawLine(0,radius + scaleToRingSpace
-//                        ,0,radius + scaleToRingSpace + scaleLine
-//                        ,specialScalePaint);
-//            }
-//            //画未滑动到的刻度线
-//            else{
-//                canvas.drawLine(0,radius + scaleToRingSpace
-//                        ,0,radius + scaleToRingSpace + scaleLine
-//                        ,scalePaint);
-//            }
-//            canvas.rotate(sweepAngle/(scaleLineCount * 1f));
-//        }
         //操作完成后恢复状态
         canvas.restore();
     }
@@ -339,13 +303,13 @@ public class MySeekBar extends View {
         int y = (int) event.getY();
         switch (action) {
             case MotionEvent.ACTION_DOWN:
-                if (isOnRing(x, y) && y <= radius + getPaddingTop() + specialScaleLineLength + scaleToRingSpace) {
+                if (isOnRing(x, y) && y <= radius + getPaddingTop()) {
                     updateProgress(x, y);
                     return true;
                 }
                 break;
             case MotionEvent.ACTION_MOVE:
-                if (y <= radius + getPaddingTop() + specialScaleLineLength + scaleToRingSpace) {
+                if (y <= radius + getPaddingTop()) {
                     updateProgress(x, y);
                 }
                 return true;
@@ -360,8 +324,8 @@ public class MySeekBar extends View {
      * 根据当前点的位置求角度，再转换成当前进度
      */
     private void updateProgress(int eventX, int eventY) {
-        double angle = Math.atan2(eventY - (radius + getPaddingLeft() + specialScaleLineLength + scaleToRingSpace)
-                , eventX - (radius + getPaddingLeft() + specialScaleLineLength + scaleToRingSpace)) / Math.PI;
+        double angle = Math.atan2(eventY - (radius + getPaddingLeft())
+                , eventX - (radius + getPaddingLeft())) / Math.PI;
         angle = ((2 + angle) % 2 + (-beginLocation / 180f)) % 2;
         if ((int) Math.round(angle * 100) >= 0) {
             progress = (int) Math.round(angle * 100);
@@ -375,9 +339,9 @@ public class MySeekBar extends View {
      */
     private boolean isOnRing(float eventX, float eventY) {
         boolean result = false;
-        double distance = Math.sqrt(Math.pow(eventX - (radius + getPaddingLeft() + specialScaleLineLength + scaleToRingSpace), 2)
-                + Math.pow(eventY - (radius + getPaddingLeft() + specialScaleLineLength + scaleToRingSpace), 2));
-        if (distance < (2 * radius + getPaddingLeft() + getPaddingRight() + 2 * (specialScaleLineLength + scaleToRingSpace))
+        double distance = Math.sqrt(Math.pow(eventX - (radius + getPaddingLeft()), 2)
+                + Math.pow(eventY - (radius + getPaddingLeft()), 2));
+        if (distance < (2 * radius + getPaddingLeft() + getPaddingRight())
                 && distance > radius - slideAbleLocation) {
             result = true;
         }
